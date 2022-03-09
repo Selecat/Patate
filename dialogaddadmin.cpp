c4984bd912de7b5f278f3e4df57e230789247b6f
Selecat <lopez.jonathan04000@gmail.com>
2022-03-09 17:33:49 +0100

    avancé sur l'ajout d'Admin par le Super admin

--- dialogaddadmin.cpp
+++ dialogaddadmin.cpp
@@ -15,13 +15,22 @@ DialogAddAdmin::~DialogAddAdmin()
 
 void DialogAddAdmin::on_pushButtonAddAdministrator_clicked()
 {
-    QString reqAddAdmin = "INSERT INTO Employe loginEmploye, PassEmploye, nomEmploye, PrenomEmploye, TelEmploye, MailEmploye, rueEmploye, villeEmploye, CpEmploye";
+    //récupération des donées
+    QString loginAdmin=ui->lineEditLoginAddAdmin->text();
+    QString passAdmin=ui->lineEditPassAddAdmin->text();
+    QString nameAdmin=ui->lineEditNameAddAdmin->text();
+    QString firstName=ui->lineEditFNameAddAdmin->text();
+    QString phoneAdmin=ui->lineEditPhoneAddAdmin->text();
+    QString mailAdmin=ui->lineEditMailAddAdmin->text();
+    QString streetAdmin=ui->lineEditStreetAddAdmin->text();
+    QString cityAdmin=ui->lineEditCityAddAdmin->text();
+    QString cpAdmin=ui->lineEditDepartementAddAdmin->text();
+
+    QString reqAddAdmin = "INSERT INTO Employe loginEmploye, PassEmploye, nomEmploye, PrenomEmploye, TelEmploye, MailEmploye, rueEmploye, villeEmploye, CpEmploye Values("+loginAdmin+", password("+passAdmin+"), "+nameAdmin+", "+firstName+", "+phoneAdmin+", "+mailAdmin+", "+streetAdmin+", "+cityAdmin+", "+cpAdmin+")";
 
 }
-
 
 void DialogAddAdmin::on_pushButtonCancelAddAdministrator_clicked()
 {
     close();
 }
-
